cabdbb4d06d9a68044417697b0cf72ce2206104c
Esteban Raffo <e.fraffo@gmail.com>
2020-11-28 18:43:35 -0300

    Practico 8. Ejercicio 3. Método calcularDoble().

---
+++ src/main/java/Ejercicio3/ejercicio3.java
@@ -0,0 +1,12 @@
+package Ejercicio3;
+
+public class ejercicio3 {
+
+    public static void main(String args[]){
+
+    }
+
+    public static float calcularDoble(int unNumero){
+        return unNumero * 2;
+    }
+}
